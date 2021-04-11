6e22b43a548e1ded10ef0f8cd717edf57a64f5c7
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2021-04-11 11:09:00 +0700

    dawnbreaker

--- lua/autorun/arccw_slog_tuna_sound.lua
+++ lua/autorun/arccw_slog_tuna_sound.lua
@@ -153,3 +153,74 @@ sound.Add({
 	volume = 		1.0,
 	sound = {"weapons/arccw_slog/fortuna/snip/bolt2.wav"}
 })
+
+
+//DB14
+sound.Add({
+	name = 			"ArcCW_Slog_FTuna_Snip.Foley1",
+	channel = 		CHAN_ITEM3,
+	volume = 		1.0,
+	sound = {"weapons/arccw_slog/fortuna/shotgun/foley1.wav"}
+})
+sound.Add({
+	name = 			"ArcCW_Slog_FTuna_Snip.Foley2",
+	channel = 		CHAN_ITEM7,
+	volume = 		1.0,
+	sound = {"weapons/arccw_slog/fortuna/shotgun/foley2.wav"}
+})
+sound.Add({
+	name = 			"ArcCW_Slog_FTuna_Snip.Foley3",
+	channel = 		CHAN_ITEM8,
+	volume = 		1.0,
+	sound = {"weapons/arccw_slog/fortuna/shotgun/foley2.wav"}
+})
+sound.Add({
+	name = 			"ArcCW_Slog_FTuna_Snip.Foley4",
+	channel = 		CHAN_ITEM9,
+	volume = 		1.0,
+	sound = {"weapons/arccw_slog/fortuna/shotgun/foley4.wav"}
+})
+sound.Add({
+	name = 			"ArcCW_Slog_FTuna_shotgun.Pump1",
+	channel = 		CHAN_ITEM1,
+	volume = 		1.0,
+	sound = {
+	"weapons/arccw_slog/fortuna/shotgun/pump1a.wav",
+	"weapons/arccw_slog/fortuna/shotgun/pump1b.wav",
+	"weapons/arccw_slog/fortuna/shotgun/pump1c.wav",
+	}
+})
+sound.Add({
+	name = 			"ArcCW_Slog_FTuna_shotgun.Pump2",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = {
+	"weapons/arccw_slog/fortuna/shotgun/pump2a.wav",
+	"weapons/arccw_slog/fortuna/shotgun/pump2b.wav",
+	"weapons/arccw_slog/fortuna/shotgun/pump2c.wav",
+	"weapons/arccw_slog/fortuna/shotgun/pump2d.wav",	
+	}
+})
+sound.Add({
+	name = 			"ArcCW_Slog_FTuna_Shotgun.In",
+	channel = 		CHAN_ITEM3,
+	volume = 		1.0,
+	sound = {
+	"weapons/arccw_slog/fortuna/shotgun/in1.wav",
+	"weapons/arccw_slog/fortuna/shotgun/in2.wav",
+	"weapons/arccw_slog/fortuna/shotgun/in3.wav",
+	"weapons/arccw_slog/fortuna/shotgun/in4.wav",	
+	}
+})
+sound.Add({
+	name = 			"ArcCW_Slog_FTuna_shotgun.Pump3",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = {"weapons/arccw_slog/fortuna/shotgun/pump3.wav"}
+})
+sound.Add({
+	name = 			"ArcCW_Slog_FTuna_shotgun.Pump4",
+	channel = 		CHAN_ITEM,
+	volume = 		1.0,
+	sound = {"weapons/arccw_slog/fortuna/shotgun/pump4.wav"}
+})
---
+++ lua/weapons/arccw_slog_tuna_mosler.lua
@@ -0,0 +1,241 @@
+SWEP.Base = "arccw_base"
+SWEP.Spawnable = true -- this obviously has to be set to true
+SWEP.Category = "ArcCW - ForTuna" -- edit this if you like
+SWEP.AdminOnly = false
+
+SWEP.PrintName = "Dawn Breaker-14D"
+SWEP.Trivia_Class = "Shotgun"
+SWEP.Trivia_Desc = "Lever action shotgun."
+SWEP.Trivia_Manufacturer = "DS Arms"
+SWEP.Trivia_Calibre = "10 Gauge"
+SWEP.Trivia_Country = "Unknown"
+SWEP.Trivia_Year = "2165"
+
+SWEP.Slot = 2
+
+SWEP.CrouchPos = Vector(-1, 2, -1)
+SWEP.CrouchAng = Angle(0, 0, -15)
+
+SWEP.NPCWeaponType = "weapon_ar2"
+SWEP.NPCWeight = 250
+
+SWEP.CamAttachment = 3
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/weapons/arccw/slog_osi_suck/c_dawnbreaker.mdl"
+SWEP.WorldModel = "models/weapons/arccw/slog_osi_suck/c_dawnbreaker.mdl"
+SWEP.ViewModelFOV = 70
+
+SWEP.Damage = 12
+SWEP.DamageMin = 5 -- damage done at maximum range
+SWEP.Range = 70 -- in METRES
+SWEP.Penetration = 5
+SWEP.DamageType = DMG_BULLET + DMG_BLAST + DMG_AIRBOAT
+SWEP.ShootEntity = nil -- entity to fire, if any
+SWEP.MuzzleVelocity = 1500 -- projectile or phys bullet muzzle velocity
+-- IN M/S
+
+SWEP.TracerNum = 1 -- tracer every X
+SWEP.TracerCol = Color(255, 25, 25)
+SWEP.TracerWidth = 3
+
+SWEP.ChamberSize = 0 -- how many rounds can be chambered.
+SWEP.Primary.ClipSize = 6 -- DefaultClip is automatically set.
+SWEP.MaxRecoilBlowback = 5
+
+SWEP.Recoil = 2
+SWEP.RecoilSide = 2
+SWEP.RecoilRise = 1.5
+SWEP.VisualRecoilMult = 0.8
+
+SWEP.Delay = 60 / 120 -- 60 / RPM.
+SWEP.Num = 15 -- number of shots per trigger pull.
+
+SWEP.Firemodes = {
+	{
+        Mode = 1,
+    },	
+    {
+        Mode = 0
+    }
+}
+
+SWEP.AccuracyMOA = 50 -- accuracy in Minutes of Angle. There are 60 MOA in a degree.
+SWEP.HipDispersion = 350 -- inaccuracy added by hip firing.
+SWEP.MoveDispersion = 300
+
+SWEP.Primary.Ammo = "ar2" 
+
+SWEP.ShootVol = 110 -- volume of shoot sound
+SWEP.ShootPitch = 100 -- pitch of shoot sound
+
+SWEP.ShootSound = "weapons/arccw_slog/fortuna/snip/fire.wav"
+SWEP.ShootSoundSilenced = "weapons/arccw_slog/fortuna/snip/sd.wav"
+SWEP.DistantShootSound = "weapons/arccw_slog/fortuna/rifle/echo.wav"
+
+SWEP.MuzzleEffect = "muzzleflash_pistol"
+SWEP.ShellModel = "models/shells/shell_9mm.mdl"
+SWEP.ShellScale = 1.5
+
+SWEP.MuzzleEffectAttachment = 1 -- which attachment to put the muzzle on
+SWEP.CaseEffectAttachment = 2 -- which attachment to put the case effect on
+
+SWEP.SightTime = 0.3
+
+SWEP.SpeedMult = 0.9
+SWEP.SightedSpeedMult = 0.75
+
+SWEP.BarrelLength = 20
+
+SWEP.ProceduralRegularFire = false
+SWEP.ProceduralIronFire = false
+
+SWEP.CaseBones = {}
+
+SWEP.IronSightStruct = {
+    Pos = Vector(-3.441, -1, 0.519),
+    Ang = Angle(0.945, 0, -5),
+    Magnification = 1.1,
+    SwitchToSound = "", -- sound that plays when switching to this sight
+}
+
+SWEP.HoldtypeHolstered = "passive"
+SWEP.HoldtypeActive = "ar2"
+SWEP.HoldtypeSights = "rpg"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_PISTOL
+
+SWEP.ActivePos = Vector(0, 3, 0.5)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.HolsterPos = Vector(-2.8, 3.247, 0.239)
+SWEP.HolsterAng = Angle(-13.101, 7.586, -16.496)
+
+SWEP.CustomizePos = Vector(4, -2, 1)
+SWEP.CustomizeAng = Angle(0 , 21.236, 17)
+
+SWEP.BarrelOffsetSighted = Vector(0, 0, -1)
+SWEP.BarrelOffsetHip = Vector(2, 0, -2)
+SWEP.ShellRotateAngle = Angle(0, -90, 40)
+
+SWEP.ExtraSightDist = 2.5
+
+SWEP.ManualAction = true
+SWEP.NoLastCycle = true
+
+SWEP.AttachmentElements = {		
+    ["iron_no"] = {
+        VMBodygroups = {
+            {ind = 1, bg = 1},
+        },
+    },	
+}
+
+SWEP.WorldModelOffset = {
+    pos = Vector(-4.8, 5.5, -4.5),
+    ang = Angle(-10, 0, 180-5)
+}
+
+SWEP.MirrorVMWM = true
+SWEP.GuaranteeLaser = true
+SWEP.Attachments = {
+    {
+        PrintName = "Optic",
+        DefaultAttName = "Iron Sights",
+        Slot = "fortuna_optic",
+        Bone = "W_Main", 
+        Offset = {
+            vpos = Vector(0, -4.1, 2.5), 
+            vang = Angle(90, 0, -90),
+        },			
+        InstalledEles = {"iron_no"},		
+    },
+    {
+        PrintName = "Muzzle",
+        DefaultAttName = "Standard Muzzle",
+        Slot = "fortuna_muzzle",
+        Bone = "W_Main",
+        Offset = {
+            vpos = Vector(0, -3.1, 17),
+            vang = Angle(90, 0, -90),
+        },
+    },
+    {
+        PrintName = "Tactical",
+        Slot = "fortuna_tac",
+        Bone = "W_Main",
+        Offset = {
+            vpos = Vector(0, -1.25, 4.5), 
+            vang = Angle(90, 0, -90),
+        },
+        ExtraSightDist = 10,
+        CorrectivePos = Vector(0.75, 3, 0),					
+    },
+    {
+        PrintName = "Ammo Type",
+        Slot = {"ammo_bullet"}
+    },
+    {
+        PrintName = "Perk",
+        Slot = {"perk"}
+    },	
+    {
+        PrintName = "Charm",
+        DefaultAttName = "None",
+        Slot = {"charm"},
+        Bone = "Weapon_Bolt",
+        Offset = {
+            vpos = Vector(0.5, 0.2, 2),
+            vang = Angle(90, 0, -90),
+            wpos = Vector(8, 1, -3),
+            wang = Angle(-9, 0, 180)
+        },
+		FreeSlot = true,
+    },	
+}
+SWEP.ShotgunReload = true
+SWEP.Animations = {
+	["idle"] = {Source = "idle",},
+    ["ready"] = {
+        Source = "draw",
+    },
+    ["draw"] = {
+        Source = "draw",
+    },
+    ["fire"] = {
+        Source = "fire",
+		MinProgress = 0.15,
+    },
+    ["cycle"] = {
+        Source = {"pump1","pump1","pump2"},
+        ShellEjectAt = 0.3,
+    },
+    ["cycle_iron"] = {
+        Source = {"pump1"},
+        ShellEjectAt = 0,
+    },	
+    ["fire_iron"] = {
+        Source = "idle",
+        ShellEjectAt = 0,
+    },
+    ["sgreload_start"] = {
+        Source = "start",
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,
+    },
+    ["sgreload_start_empty"] = {
+        Source = "dry",
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,
+        LHIK = true,
+        LHIKIn = 0.5,
+        LHIKOut = 0,
+    },
+    ["sgreload_insert"] = {
+        Source = "reload",
+        TPAnim = ACT_HL2MP_GESTURE_RELOAD_SHOTGUN,
+        TPAnimStartTime = 0.3,
+    },
+    ["sgreload_finish"] = {
+        Source = "end",
+    },
+}
