36ecbac089de003a1ce0642fd0d1cee73372ad5d
mammal is fleshy <69098037+mammal-is-fleshy@users.noreply.github.com>
2021-07-01 17:22:13 +0700

    incase the big fuckoff glowing head isnt enough, added more glowing spot

--- lua/arccw/documentation/fortuna_credits.lua
+++ lua/arccw/documentation/fortuna_credits.lua
@@ -62,6 +62,8 @@ Laser (low priority)
 HL-877, Cmd97 SOUNDS (low priority)
 Leviathan idle, remaining sound (low priority)
 
+NPC exclusive weapon (very very very low priority)
+
 Stationary MG (scrapped) -- couldnt do another belf fed,               -unless
 SMG_DMR Hybrid (scrapped??) -- i just realise how useless it is
 
